d12420e666f27150a075b5494f40559072b96bfe
Anthony Duran <87147191+Code1Here@users.noreply.github.com>
2022-12-18 01:56:55 -0500

    Update getStockPrices.py

--- getStockPrices.py
+++ getStockPrices.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
-
 tsla = 'https://finance.yahoo.com/quote/TSLA'
 fubo = 'https://finance.yahoo.com/quote/FUBO'
 docu = 'https://finance.yahoo.com/quote/DOCU'
